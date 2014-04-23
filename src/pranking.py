320e0f24d551ac283f697137206c4948dbecb2b8
bruno <bruno@superciberbruno.(none)>
2014-04-23 18:55:28 +0200

    Added license notice.

--- src/pranking.py
+++ src/pranking.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env
 
+###########################################################################
+#
+#  Copyright 2014 Bruno Ordozgoiti     
+#  Distributed under GNU GPLv3         
+#
+#  This file is part of Pranking.
+#    
+#  Pranking is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  Pranking is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with Pranking.  If not, see <http://www.gnu.org/licenses/>.
+#
+###########################################################################
+
 from __future__ import print_function
 import subprocess
 import urllib2
@@ -9,8 +31,6 @@ import datetime
 import sys
 import getopt
 from array import array
-
-
 
 class Committer:
     def __init__(self, name, count):
